62f6af834b123c539db8c5e11e0e8a65ac22cb2a
FranklinThree <franklin3@hdu.edu.cn>
2021-09-24 21:02:55 +0800

    Push in go:2

--- iotest02.go
+++ iotest02.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	//"io"
 	"os"
 )
-//一个成功的输入输出样例
-func main(){
 
-	reader,_ := os.Open("1")
-	writer,_ := os.Create("2")
+// Copy 一个成功的拷贝方法
+func Copy(source string,target string)(info int){
+	info = 1
+	reader,_ := os.Open(source)
+	writer,_ := os.Create(target)
 	var buffer = make([]byte,1024)
 	for true{
 		n,_ := reader.Read(buffer)
@@ -20,9 +20,10 @@ func main(){
 			writer.Write(buffer[:n])
 
 	}
-
+	info = 0
 	defer func(){
 		writer.Close()
 		reader.Close()
 	}()
+	return
 }
--- structure.go
+++ structure.go
@@ -20,19 +20,19 @@ type ExampleSet struct{
 }
 
 // Maintain 人脸实例集合：维护
-func (es *ExampleSet) Maintain(){
+func (es ExampleSet) Maintain(){
 
 }
 
 // AddExample 人脸实例集合：添加实例
-func (es *ExampleSet)AddExample(example Example)  {
+func (es ExampleSet)AddExample(example Example)  {
 	es.examples[es.length] = example
 	es.indexes[es.length] = es.length
 	es.length++
 }
 
 // GetExample 人脸实例集合：取得指定实例
-func (es *ExampleSet)GetExample(index int)Example{
+func (es ExampleSet) GetExample (index int)Example{
 	//异常：人脸实例集合中无实例
 	if es.length == 0 {
 		return Example{}
@@ -49,6 +49,5 @@ func (es *ExampleSet)GetRandomExample()(Example){
 	if es.length == 0 {
 		return Example{}
 	}
-
 	return  es.GetExample(rand.Intn(es.length))
 }
