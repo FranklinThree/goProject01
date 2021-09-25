822e6e51fd4c1a19b497097a670b84e9587c5a7e
FranklinThree <franklin3@hdu.edu.cn>
2021-09-25 17:38:51 +0800

    Push in go:5

--- test/httpservertest01.go
+++ test/httpservertest01.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+func HelloHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,"Hello world")
+}
+func main(){
+	http.HandleFunc("/",HelloHandler)
+	http.ListenAndServe("127.0.0.1:33333",nil)
+}
--- test/httpservertest02.go
+++ test/httpservertest02.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HelloHandlerStruct struct {
+	content string
+}
+
+func (handler *HelloHandlerStruct) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w,handler.content)
+
+}
+
+func main(){
+	http.Handle("/",&HelloHandlerStruct{"Hello World"})
+	http.ListenAndServe("127.0.0.1:6666",nil)
+}
